26f95cb70bb7166fa4032fa444ed9c3c42f16298
alexandrems7 <alexandremartins770@gmail.com>
2022-08-28 14:59:05 -0300

    configs in middleware

--- src/auth/auth.middleware.js
+++ src/auth/auth.middleware.js
@@ -7,26 +7,26 @@ module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).send({ message: "O token não foi informado!" });
+    return res.status(401).send({ message: "Você não informou nenhum token." });
   }
 
   const parts = authHeader.split(" ");
 
   if (parts.length !== 2) {
-    return res.status(401).send({ message: "Token inválido" });
+    return res.status(401).send({ message: "Poxa! Esse token é inválido!" });
   }
 
   const [scheme, token] = parts;
 
   if (!/^Bearer^/i.test(scheme)) {
-    return res.status(401).send({ message: "Token mal formatado!" });
+    return res.status(401).send({ message: "Caraca, amigão! Token mal-formatado." });
   }
 
   jwt.verify(token, process.env.SECRET, async (err, decoded) => {
     const user = await findByIdUserService(decoded.id);
 
     if(err || !user || !user.id){
-        return res.status(401).send({ message: "Token inválido!" });
+        return res.status(401).send({ message: "Poxa! Esse token é inválido!" });
     }
 
     req.userId = user.id;
