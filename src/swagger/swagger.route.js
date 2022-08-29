746e79d4776016bf9b18215043470c21a6d13ead
alexandrems7 <alexandremartins770@gmail.com>
2022-08-28 21:08:05 -0300

    remote server swagger

--- src/swagger/swagger.route.js
+++ src/swagger/swagger.route.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger");
 
-route.use("/", swaggerUi.serve);
-route.get("/", swaggerUi.setup(swaggerDocument));
+
 
 module.exports = router;
